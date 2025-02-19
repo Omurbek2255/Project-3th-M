e926f29dd45463b093baa758eeccf24cef0a8339
omurbek_on <kadyrov12.on@gmail.com>
2025-02-19 19:07:44 +0600

    converter hw

--- js/lesson.js
+++ js/lesson.js
@@ -15,3 +15,78 @@ phoneButton.onclick = () =>{
         phoneResult.style.color = 'red'
     }
 }
+
+
+// CONVERTER
+
+
+const somInput = document.querySelector('#som')
+const usdInput = document.querySelector('#usd')
+const eurInput = document.querySelector('#eur')
+
+
+const converter = (element, targetElement1, targetElement2 ) =>{
+    element.oninput = () =>{
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json')
+        request.setRequestHeader('Comtent-type','application/json')
+        request.send()
+
+        request.onload = () =>{
+            const data = JSON.parse(request.response)
+            if (element.id === 'usd') {
+                targetElement1.value = (element.value * data.usdXsom).toFixed(2);
+                targetElement2.value = (element.value / data.eurXusd).toFixed(2);
+                
+            }
+            if(element.id === 'som'){
+                targetElement1.value = (element.value / data.usdXsom).toFixed(3);
+                targetElement2.value = (element.value / data.eurXsom).toFixed(3);
+                
+            }
+            if(element.id === 'eur'){
+                targetElement1.value = (element.value * data.eurXusd).toFixed(2);
+                targetElement2.value = (element.value * data.eurXsom).toFixed(2);
+                
+            }
+            if (element.value === '') {
+                targetElement1.value = ''
+                targetElement2.value = ''
+            }
+            
+        }
+
+        
+    }
+
+}
+
+converter(somInput, usdInput, eurInput )
+converter(usdInput, somInput, eurInput)
+converter(eurInput, usdInput, somInput)
+
+// somInput.oninput = () =>{
+//     const request = new XMLHttpRequest();
+//     request.open("GET", '../data/converter.json')
+//     request.setRequestHeader('Content-type', 'application/json')
+//     request.send()
+
+//     request.onload = () =>{
+//         const data = JSON.parse(request.response)
+//         usdInput.value = (somInput.value / data.usd).toFixed(2)
+//     }
+// }
+
+// usdInput.oninput = () =>{
+//     const request = new XMLHttpRequest();
+//     request.open("GET", '../data/converter.json')
+//     request.setRequestHeader('Content-type', 'application/json')
+//     request.send()
+
+//     request.onload = () =>{
+//         const data = JSON.parse(request.response)
+//         somInput.value = (usdInput.value * data.usd).toFixed(2)
+//     }
+// }
+
+
